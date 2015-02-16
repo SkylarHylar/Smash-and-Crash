f92744203a2b903845163712b5fd15c40fce04ff
Andrew Iobst <andrewnugget@hotmail.com>
2015-02-16 14:01:29 -0500

    Added score.

--- SmashandCrash/SmashandCrash.ino
+++ SmashandCrash/SmashandCrash.ino
@@ -15,6 +15,7 @@ const char* const menu[MENULENGTH] PROGMEM = {
 extern const byte player[];
 extern const byte platform[];
 extern const byte meteor[];
+extern const byte font3x5[];
 extern const byte font5x7[];
 void setup(){
   gb.begin();
@@ -24,6 +25,19 @@ void setup(){
   gb.display.persistence = true;
 }
 
+int playerx = 20;
+int playery = 20;
+int playerflip = NOFLIP;
+int playerxv = 2;
+int playeryv = 1;
+int playergrav = 1;
+int frames = 0;
+
+int meteorx = random(0,76);
+int meteory = 0;
+
+boolean alive = true;
+
 void loop(){
   switch(gb.menu(menu, MENULENGTH)){
     case -1: //nothing selected
@@ -32,6 +46,15 @@ void loop(){
     case 0: //Load story
       gb.display.print(F("    Loading...."));
         gb.pickRandomSeed();
+        playerx = 20;
+        playery = 20;
+        playerflip = NOFLIP;
+        playerxv = 2;
+        playeryv = 1;
+        playergrav = 1;
+        meteory = 0;
+        meteorx = random(0,76);
+        alive = true;
         play();
       break;
     case 1: //Load Survival
